73ba9900974715fbd6ff57ad09f1124db6c0d6ff
Heath Hopkins <HeathHopkins@users.noreply.github.com>
2023-01-13 14:33:09 -0500

    Create prep_machine_g4.sh

---
+++ prep_machine_g4.sh
@@ -0,0 +1,32 @@
+
+# install docker and hw tools
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin awscli nvme-cli ubuntu-drivers-common
+sudo usermod -aG docker ubuntu
+
+# install node and zx
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt install -y nodejs pass inotify-tools
+sudo npm i -g zx
+
+sudo apt-get install -y nvidia-driver-470 nvidia-utils-470
+
+# TODO: use modprobe instead of reboot?
+sudo reboot now
+
+# install nvidia-docker
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2 nvidia-container-toolkit
+sudo shutdown -r now
+
+# test gpu with nvidia-smi command
